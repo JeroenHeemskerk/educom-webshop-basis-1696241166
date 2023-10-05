db9860461124e9b3179130a87999f8ee39c7c622
Laura Bokkers <L.A.Bokkers@gmail.com>
2023-10-05 14:09:00 +0200

    validatie conditioneel gemaakt

--- contact.php
+++ contact.php
@@ -12,11 +12,17 @@
         $valid = false;
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // validate the 'POST' data
+            // validate for the 'POST' data
             if(empty($_POST["name"])){
                 $nameErr = "Name is required";
             } else {
                  $name = test_input($_POST["name"]);
+            }
+
+            if (empty($_POST["email"])){
+                $phonenumberErr = "Email is required";
+            } else {
+                $email = test_input($_POST["email"]);
             }
            
             if (empty($_POST["phonenumber"])){
@@ -31,7 +37,12 @@
                 $message = test_input($_POST["phonenumber"]);
             }
   
-            $valid = true;
+            if (empty($_POST["messageErr"])){
+                 $valid = true;
+            } else {
+                $valid = false; 
+            }
+            
          }
          
         ?> 
@@ -49,37 +60,30 @@
 
     <section>
 
-    <!-- Dit is de gekopieerde php code die in de opdracht staat-->
+    <!-- Dit is de gekopieerde (nu aangepaste) php code die in de opdracht staat-->
 
-        <?php if (!$valid) { /* Show the next part only when $valid is false */ ?>
+    <?php if (!$valid) { /* Show the next part only when $valid is false */ ?>
 
-                <form method="POST" action="contact.php">;
-                  
-                  <input name="email" value="<?php echo $email; ?>" id="email">
-                  <span class="error">* <?php echo $emailErr; ?></span>
-                  
-                </form>;
-          
-              <?php } else { /* Show the next part only when $valid is true */ ?>
-          
-               <p>Bedankt voor uw reactie:</p> 
-               
-               <div>Email: <?php echo $email; ?></div>
-               
-          
-              <?php } /* End of conditional showing */ ?>
-
-    <form>
+        <form method="POST" action="contact.php">
         <select id="salutation">
             <option value="mr">Dhr.</option>
             <option value="mrs">Mevr.</option> 
         </select> </br></br>
+
         <label for="fname">Naam:</label>
         <input type="text" name="fname" id="fname"></br>
+        <span class="error">* <?php echo $nameErr;?></span>
+        <br><br>
+
         <label for="email">Email:</label>
         <input type="text" name="email" id="email"></br>
+        <span class="error">* <?php echo $emailErr;?></span>
+        <br><br>
+
         <label for="phonenumber">Telefoonnummer:</label>
         <input type="text" name="phonenumber" id="phonenumber"></br></br>
+        <span class="error">* <?php echo $phonenumberErr;?></span>
+        <br><br>
         
         <label for="fav_communication">Communicatievoorkeur:</label>
         <input type="radio" name="fav_communication" id="communication_email">
@@ -87,10 +91,27 @@
         <input type="radio" name="fav_communication" id="communication_phone">
         <label for="phone">Telefoon</label></br></br>
 
-        <label for="contact">Contact:</label>
+        <label for="contact">Contact:</label></br>
         <textarea name="contact" id="contact" cols="40" rows="5" placeholder="Schrijf hier je bericht"></textarea></br>
+        <span class="error">* <?php echo $messageErr;?></span>
+        <br><br>
+
         <button>Verzenden</button>
-    </form></br>
+        </form></br>
+
+          
+        <?php } else { /* Show the next part only when $valid is true */ ?>
+          
+            <p>Bedankt voor uw reactie:</p> 
+               
+             <div>Name: <?php echo $salutation + $name; ?></div>
+             <div>Email: <?php echo $email; ?></div>
+             <div>Phonenumber: <?php echo $phonenumber; ?></div>
+             <div>Communication preference: <?php echo $comm_preference; ?></div>
+             <div>Your message: <?php echo $message; ?></div>
+               
+             <?php } /* End of conditional showing */ ?> 
+
     </section>
 
     <footer class="footers">
