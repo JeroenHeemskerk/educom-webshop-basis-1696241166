0bdf4cc12a9fa09ce1ffbd2b0530f29a484e9e61
Laura Bokkers <L.A.Bokkers@gmail.com>
2023-10-06 13:01:04 +0200

    Issue #5 afgerond

--- contact.php
+++ contact.php
@@ -24,46 +24,46 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         // validate for the 'POST' data
-        if (empty($_POST["saluation"])) {
-            $salutationErr = "Salutation is required";
+        if (empty($_POST["salutation"])) {
+            $salutationErr = "*Salutation is required";
         } else {
             $salutation = test_input($_POST["salutation"]);
         }
 
         if (empty($_POST["name"])) {
-            $nameErr = "Name is required";
+            $nameErr = "*Name is required";
         } else {
             $name = test_input($_POST["name"]);
             // check if name only contains letters and whitespace
             if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-                $nameErr = "Only letters and white space allowed";
+                $nameErr = "*Only letters and white space allowed";
             }
         }
 
         if (empty($_POST["email"])) {
-            $emailErr = "Email is required";
+            $emailErr = "*Email is required";
         } else {
             $email = test_input($_POST["email"]);
             // check if e-mail address is well-formed
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Invalid email format";
+                $emailErr = "*Invalid email format";
             }
         }
 
         if (empty($_POST["phonenumber"])) {
-            $phonenumberErr = "Phonenumber is required";
+            $phonenumberErr = "*Phonenumber is required";
         } else {
             $phonenumber = test_input($_POST["phonenumber"]);
         }
 
         if (empty($_POST["comm_preference"])) {
-            $comm_preferenceErr = "Communication preference is required";
+            $comm_preferenceErr = "*Communication preference is required";
         } else {
             $comm_preference = test_input($_POST["comm_preference"]);
         }
 
         if (empty($_POST["message"])) {
-            $messageErr = "Message is required";
+            $messageErr = "*Message is required";
         } else {
             $message = test_input($_POST["message"]);
         }
@@ -92,39 +92,42 @@
     <section>
 
         <?php if (!$valid) { /* Show the next part only when $valid is false */ ?>
-
+           
             <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                 <select name="salutation" id="salutation">
-                    <option value="mr">Dhr.</option>
-                    <option value="mrs">Mevr.</option>
-                    <span class="error">* <?php echo $salutationErr; ?></span>
-                </select> </br></br>
+                    <option value="mr" <?php if (isset($salutation) && $salutation=="mr") echo "selected";?>>Dhr.</option>
+                    <option value="mrs" <?php if (isset($salutation) && $salutation=="mrs") echo "selected";?>>Mevr.</option>
+                </select> </br>
+                <span class="error"> <?php echo $salutationErr; ?></span></br></br>
 
                 <label for="name">Naam:</label>
                 <input type="text" name="name" id="name" value="<?php echo $name; ?>"></br>
-                <span class="error">* <?php echo $nameErr; ?></span>
+                <span class="error"> <?php echo $nameErr; ?></span>
                 <br><br>
 
                 <label for="email">Email:</label>
                 <input type="text" name="email" id="email" value="<?php echo $email; ?>"></br>
-                <span class="error">* <?php echo $emailErr; ?></span>
+                <span class="error"> <?php echo $emailErr; ?></span>
                 <br><br>
 
                 <label for="phonenumber">Telefoonnummer:</label>
-                <input type="text" name="phonenumber" id="phonenumber" <?php echo $phonenumber; ?>></br></br>
-                <span class="error">* <?php echo $phonenumberErr; ?></span>
+                <input type="text" name="phonenumber" id="phonenumber" value="<?php echo $phonenumber; ?>"></br></br>
+                <span class="error"> <?php echo $phonenumberErr; ?></span>
                 <br><br>
 
                 <label for="comm_preference">Communicatievoorkeur:</label>
-                <input type="radio" name="comm_preference" id="communication_email" value="email">
+                
+                <input type="radio" name="comm_preference" id="communication_email" <?php if (isset($comm_preference) && $comm_preference=="email") echo "checked";?> value="email">
                 <label for="email">Email</label>
-                <input type="radio" name="comm_preference" id="communication_phone" value="phone">
+                
+                <input type="radio" name="comm_preference" id="communication_phone" <?php if (isset($comm_preference) && $comm_preference=="phone") echo "checked";?> value="phone">
                 <label for="phone">Telefoon</label></br>
-                <span class="error">* <?php echo $comm_preferenceErr; ?></span></br></br>
+                
+                <span class="error"> <?php echo $comm_preferenceErr; ?></span></br></br>
 
                 <label for="message">Contact:</label></br>
                 <textarea name="message" id="contact" cols="40" rows="5" placeholder="Schrijf hier je bericht"><?php echo $message; ?></textarea></br>
-                <span class="error">* <?php echo $messageErr; ?></span>
+                <span class="error"> <?php echo $messageErr; ?></span>
                 <br><br>
 
                 <button>Verzenden</button>
@@ -135,7 +138,7 @@
 
             <p>Bedankt voor uw reactie:</p>
 
-            <div>Name: <?php echo $salutation . $name; ?></div>
+            <div>Name: <?php echo $salutation ." ". $name; ?></div>
             <div>Email: <?php echo $email; ?></div>
             <div>Phonenumber: <?php echo $phonenumber; ?></div>
             <div>Communication preference: <?php echo $comm_preference; ?></div>
